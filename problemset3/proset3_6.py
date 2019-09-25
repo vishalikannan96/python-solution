a42864f8a640f060ea487bd17ebfc723bf3c2a40
A08019DIRP_C2E.03.06 <A08019DIRP_C2E.03.06@accenture.com>
2019-09-25 10:50:32 +0530

    set3

--- problemset3/proset3_6.py
+++ problemset3/proset3_6.py
@@ -1,3 +1,16 @@
 #Modify your program to prompt the user to enter a string of forbidden letters and then print the number
 # of words that don’t contain any of them. Can you find a combination of 5 forbidden letters that excludes
 # the smallest number of words?
+
+def avoid(list1,forbidden_str):
+    for char in list1:
+        count=0
+        for letter in forbidden_str:
+            if letter in char:
+                count+=1
+        if count==0:
+            print(char)
+string=str(input('Enter string:'))
+list1=string.split(" ")
+forbidden_str=str(input('Enter forbidden string:'))
+avoid(list1,forbidden_str)
